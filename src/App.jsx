f10443c7a095c634401a0f4ecfed0b37565c2e74
Tamilselvan Krishnamurthi <155523691+tamilselvankrishnamurthigit2197@users.noreply.github.com>
2024-11-30 13:47:41 +0530

    Update App.jsx
    issue with main.jsx because there is no default export in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,62 @@
-/* getting values */
-var popupoverlay = document.querySelector("popup-overlay");
-var popupbox = document.querySelector("popup-box");
+// Getting values
+var popupoverlay = document.querySelector(".popup-overlay");
+var popupbox = document.querySelector(".popup-box");
 var addpopupbutton = document.getElementById("add-popupbutton");
 
-/* for addpopup button */
-addpopupbutton.addEventListener("click", function(){
+// Show popup when the add button is clicked
+addpopupbutton.addEventListener("click", function () {
   popupoverlay.style.display = "block";
   popupbox.style.display = "block";
-})
+});
 
 var cancelpopup = document.getElementById("cancel-popup");
 
-/* to cancel the zoomed popup box */
-
-cancelpopup.addEventListener("click", function(event){
+// Close popup when cancel button is clicked
+cancelpopup.addEventListener("click", function (event) {
   event.preventDefault();
-
   popupbox.style.display = "none";
   popupoverlay.style.display = "none";
-})
+});
 
-var container = document.querySelector("container");
+// Select the container for books
+var container = document.querySelector(".container");
 var addBook = document.getElementById("add-book");
+
+// Input fields for book details
 var bookTitleInput = document.getElementById("book-title-input");
 var bookAuthorInput = document.getElementById("book-author-input");
 var bookDescriptionInput = document.getElementById("book-description-input");
 
-addBook.addEventListener("click", function(event){
+// Add a new book
+addBook.addEventListener("click", function (event) {
   event.preventDefault();
 
-  var div = document.createElement(div)
+  // Create a new div for the book
+  var div = document.createElement("div");
   div.setAttribute("class", "book-container");
 
-  /* inside popupbox filling */
-  div.innerHTML = `<h2>${bookTitleInput}</h2>
-                    <h5>${bookAuthorInput}</h5>
-                    <h2>${bookDescriptionInput}</h2>
-                    <button onclick={deleteBook(event)}></button>`
+  // Fill the new book container with details
+  div.innerHTML = `
+    <h2>${bookTitleInput.value}</h2>
+    <h5>${bookAuthorInput.value}</h5>
+    <p>${bookDescriptionInput.value}</p>
+    <button onclick="deleteBook(event)">Delete</button>
+  `;
+
+  // Append the new book container to the main container
   container.append(div);
-})
 
-/* delete Function */
+  // Clear the popup input fields
+  bookTitleInput.value = "";
+  bookAuthorInput.value = "";
+  bookDescriptionInput.value = "";
 
+  // Close the popup
+  popupbox.style.display = "none";
+  popupoverlay.style.display = "none";
+});
+
+// Delete a book
 function deleteBook(event) {
   event.target.parentElement.remove();
 }
